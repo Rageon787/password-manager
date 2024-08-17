e7e2ab0d4e6faf53806a5762678e510e07930b7e
Ragav Srinivasan <56784771+Rageon787@users.noreply.github.com>
2024-08-17 12:15:48 +1000

    implemented widgets for _create_editPane()

--- src/main.py
+++ src/main.py
@@ -71,8 +71,30 @@ class Config:
 
         # Triggers when a user wants to edit an item 
 
-        self._empty_configFrame()
+        self._empty_configFrame() 
         ttk.Label(self.config_frame, text = "EDIT ITEM").pack() 
+
+        nameEntry_label = ttk.Label(self.config_frame, text = "Name") 
+        nameEntry_label.pack()
+
+        name_var = tk.StringVar()
+        name_entry = ttk.Entry(self.config_frame, textvariable = name_var) 
+        name_entry.pack()  
+
+        usernameEntry_label = ttk.Label(self.config_frame, text = "Username")  
+        usernameEntry_label.pack() 
+
+        username_var = tk.StringVar()
+        username_entry = ttk.Entry(self.config_frame, textvariable = username_var) 
+        username_entry.pack()
+        
+        passwordEntry_label = ttk.Label(self.config_frame, text = "Password")  
+        passwordEntry_label.pack()  
+
+        password_var = tk.StringVar()
+        password_entry = ttk.Entry(self.config_frame, textvariable = password_var) 
+        password_entry.pack() 
+
 
     def _create_newLoginPane(self): 
         
